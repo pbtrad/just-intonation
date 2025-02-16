643644c1f089405ecdc2bac5f7b4612bcdf97d74
paul <paulbrowne2@gmail.com>
2025-02-16 01:14:45 +0000

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+
+package main
+
+
+func main() {
+	// Command line flags for different modes
+
+
+	// Initialize audio system
+
+
+	// Start audio capture
+
+	// Main practice loop
+
+
+// Drone func
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pbtrad/just-intonation
+
+go 1.23.6
+
+require github.com/gordonklaus/portaudio v0.0.0-20250206071425-98a94950218b // indirect
